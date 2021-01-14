8223e9026d57b452d18675c341260224b77fb741
shono <>
2021-01-14 15:45:26 -0800

    Support the new S1 domain.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ class Model {
     this.model = await tf.loadGraphModel(modelUrl, options);
     this.input_size = this.model.inputs[0].shape[1];
     this.is_new_od_model = this.model.outputs.length == 3;
+    this.is_rgb_input = (this.model.metadata && this.model.metadata['Image.BitmapPixelFormat'] == 'Rgb8') || this.is_new_od_model;
   }
 
   dispose() {
@@ -21,10 +22,12 @@ class Model {
    * The input parameter can be a tf.Tensor or encoded bytes of an image.
    */
   async executeAsync(pixels) {
-    const inputs = pixels instanceof tf.Tensor ? pixels : this._preprocess(tf.node.decodeImage(pixels, 3));
+    const inputs = tf.tidy(() => { return pixels instanceof tf.Tensor ? pixels : this._preprocess(tf.node.decodeImage(pixels, 3)); });
     const outputs = await this.model.executeAsync(inputs, this.is_new_od_model ? this.NEW_OD_OUTPUT_TENSORS : null);
-    const arrays = !Array.isArray(outputs) ? outputs.array() : Promise.all(outputs.map(t => t.array()));
-    return await this._postprocess(await arrays);
+    tf.dispose(inputs);
+    const arrays = await (!Array.isArray(outputs) ? outputs.array() : Promise.all(outputs.map(t => t.array())));
+    tf.dispose(outputs);
+    return this._postprocess(arrays);
   }
 
   async _postprocess(outputs) {
@@ -41,7 +44,7 @@ export class ObjectDetectionModel extends Model {
 
   _preprocess(image) {
     const rgb_image = tf.image.resizeBilinear(image.expandDims().toFloat(), [this.input_size, this.input_size]);
-    return this.is_new_od_model ? rgb_image : rgb_image.reverse(-1); // RGB->BGR for old models
+    return this.is_rgb_input ? rgb_image : rgb_image.reverse(-1); // RGB->BGR for old models
   }
 
   async _postprocess(outputs) {
@@ -50,46 +53,55 @@ export class ObjectDetectionModel extends Model {
     }
 
     // TODO: Need more efficient implmentation
-    const num_anchor = this.ANCHORS.length / 2;
-    const channels = outputs[0][0][0].length;
-    const height = outputs[0].length;
-    const width = outputs[0][0].length;
+    const [boxes, scores, classes] = tf.tidy(() => {
+      const num_anchor = this.ANCHORS.length / 2;
+      const channels = outputs[0][0][0].length;
+      const height = outputs[0].length;
+      const width = outputs[0][0].length;
 
-    const num_class = channels / num_anchor - 5;
+      const num_class = channels / num_anchor - 5;
 
-    let boxes = [];
-    let scores = [];
-    let classes = [];
+      let boxes = [];
+      let scores = [];
+      let classes = [];
 
-    for (var grid_y = 0; grid_y < height; grid_y++) {
-      for (var grid_x = 0; grid_x < width; grid_x++) {
-        let offset = 0;
+      for (var grid_y = 0; grid_y < height; grid_y++) {
+        for (var grid_x = 0; grid_x < width; grid_x++) {
+          let offset = 0;
 
-        for (var i = 0; i < num_anchor; i++) {
-          let x = (this._logistic(outputs[0][grid_y][grid_x][offset++]) + grid_x) / width;
-          let y = (this._logistic(outputs[0][grid_y][grid_x][offset++]) + grid_y) / height;
-          let w = Math.exp(outputs[0][grid_y][grid_x][offset++]) * this.ANCHORS[i * 2] / width;
-          let h = Math.exp(outputs[0][grid_y][grid_x][offset++]) * this.ANCHORS[i * 2 + 1] / height;
+          for (var i = 0; i < num_anchor; i++) {
+            let x = (this._logistic(outputs[0][grid_y][grid_x][offset++]) + grid_x) / width;
+            let y = (this._logistic(outputs[0][grid_y][grid_x][offset++]) + grid_y) / height;
+            let w = Math.exp(outputs[0][grid_y][grid_x][offset++]) * this.ANCHORS[i * 2] / width;
+            let h = Math.exp(outputs[0][grid_y][grid_x][offset++]) * this.ANCHORS[i * 2 + 1] / height;
 
-          let objectness = tf.scalar(this._logistic(outputs[0][grid_y][grid_x][offset++]));
-          let class_probabilities = tf.tensor1d(outputs[0][grid_y][grid_x].slice(offset, offset + num_class)).softmax();
-          offset += num_class;
+            let objectness = tf.scalar(this._logistic(outputs[0][grid_y][grid_x][offset++]));
+            let class_probabilities = tf.tensor1d(outputs[0][grid_y][grid_x].slice(offset, offset + num_class)).softmax();
+            offset += num_class;
 
-          class_probabilities = class_probabilities.mul(objectness);
-          let max_index = class_probabilities.argMax();
-          boxes.push([x - w / 2, y - h / 2, x + w / 2, y + h / 2]);
-          scores.push(class_probabilities.max().dataSync()[0]);
-          classes.push(max_index.dataSync()[0]);
+            class_probabilities = class_probabilities.mul(objectness);
+            let max_index = class_probabilities.argMax();
+            boxes.push([x - w / 2, y - h / 2, x + w / 2, y + h / 2]);
+            scores.push(class_probabilities.max().dataSync()[0]);
+            classes.push(max_index.dataSync()[0]);
+          }
         }
       }
-    }
 
-    boxes = tf.tensor2d(boxes);
-    scores = tf.tensor1d(scores);
-    classes = tf.tensor1d(classes);
+      boxes = tf.tensor2d(boxes);
+      scores = tf.tensor1d(scores);
+      classes = tf.tensor1d(classes);
+
+      return [boxes, scores, classes];
+    });
 
     const selected_indices = await tf.image.nonMaxSuppressionAsync(boxes, scores, 10);
-    return [await boxes.gather(selected_indices).array(), await scores.gather(selected_indices).array(), await classes.gather(selected_indices).array()];
+    const tensor_results = [boxes.gather(selected_indices), scores.gather(selected_indices), classes.gather(selected_indices)];
+    const results = [await tensor_results[0].array(), await tensor_results[1].array(), await tensor_results[2].array()];
+    tf.dispose([boxes, scores, classes])
+    tf.dispose(selected_indices);
+    tf.dispose(tensor_results);
+    return results;
   }
 
   _logistic(x) {
